80a58c60ddcec8ee13d8b9af460922aaef321e5b
KimuGenie <baby11686@naver.com>
2020-12-18 22:29:18 +0900

    NN update
    upsampling을 train/test set으로 나눈 뒤 train set을 upsampling하는 방식으로 바꿈.
    loss function을 binary crossentropy로 바로잡았고 따라서 마지막 레이어 유닛 수도 바꿈.
    loss, accuracy 그래프와 test set의 confusion matrix 만듦.

--- NN.py
+++ NN.py
@@ -9,25 +9,20 @@ from sklearn.utils import resample
 df = pd.read_csv(os.path.abspath('dataset.csv'),header=None)
 X = df.iloc[:, :11].values
 y = df.iloc[:, 11].values
-X_org = X #upsampling 하지 않을 원본 데이터
-y_org = y
-
-X_upsampled, y_upsampled = resample(X[y == 1], y[y == 1], replace=True, n_samples=X[y == 0].shape[0], random_state=1)
-X = np.vstack((X[y==0], X_upsampled))
-y = np.hstack((y[y==0], y_upsampled))
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y) #30% test set
-X_train_org, X_test_org, y_train_org, y_test_org = train_test_split(X_org, y_org, test_size=0.3, random_state=1, stratify=y_org)
+
+X_upsampled, y_upsampled = resample(X_train[y_train == 1], y_train[y_train == 1], replace=True, n_samples=X_train[y_train == 0].shape[0], random_state=1)
+X_train = np.vstack((X_train[y_train==0], X_upsampled))
+y_train = np.hstack((y_train[y_train==0], y_upsampled))
 
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
-X_org_std = sc.transform(X_org)
 
-y_train_onehot = tf.keras.utils.to_categorical(y_train)
 ################################################################################
 
 np.random.seed(1)
@@ -40,7 +35,7 @@ model.add(
         input_dim=X_train_std.shape[1],
         kernel_initializer='glorot_uniform',
         bias_initializer='zeros',
-        activation='tanh'
+        activation='relu'
     )
 )
 
@@ -50,49 +45,74 @@ model.add(
         input_dim=6,
         kernel_initializer='glorot_uniform',
         bias_initializer='zeros',
-        activation='tanh'
+        activation='relu'
     )
 )
 
+#binary classification을 위해 마지막 레이어는 유닛 하나에 activation function으로 sigmoid function을 사용함
 model.add(
     tf.keras.layers.Dense(
-        units=6,
+        units=1,
         input_dim=6,
         kernel_initializer='glorot_uniform',
         bias_initializer='zeros',
-        activation='tanh'
-    )
-)
-
-model.add(
-    tf.keras.layers.Dense(
-        units=y_train_onehot.shape[1],
-        input_dim=6,
-        kernel_initializer='glorot_uniform',
-        bias_initializer='zeros',
-        activation='softmax'
+        activation='sigmoid'
     )
 )
 
 # model.summary()
 
-sgd_optimizer = tf.keras.optimizers.SGD(lr=0.003, decay=1e-7, momentum=.9)
+sgd_optimizer = tf.keras.optimizers.SGD(lr=0.01, decay=1e-7, momentum=.9)
 
-model.compile(optimizer=sgd_optimizer, loss='categorical_crossentropy')
+model.compile(optimizer=sgd_optimizer, loss='binary_crossentropy', metrics=['accuracy']) #0 또는 1로 분류하기 때문에 binary_crossentropy를 loss함수로 선정
 
-history = model.fit(X_train_std, y_train_onehot, batch_size=50, epochs=50, verbose=1, validation_split=0.1)
+history = model.fit(X_train_std, y_train, batch_size=50, epochs=50, verbose=1, validation_split=0.1)
 
-y_train_pred = model.predict_classes(X_train_std, verbose=0)
-correct_preds = np.sum(y_train == y_train_pred, axis=0)
-train_acc = correct_preds/y_train.shape[0]
-print('train accuracy:  %0.3f' %(train_acc*100))
+# history_dict = history.history
+# print(history_dict.keys())
+
+import matplotlib.pyplot as plt
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = np.arange(1, len(acc)+1)
+
+plt.plot(epochs, loss, label='Training loss')
+plt.plot(epochs, val_loss, label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+plt.plot(epochs, acc, label='Training accuracy')
+plt.plot(epochs, val_acc, label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('accuracy')
+plt.legend()
+plt.show()
+
 y_test_pred = model.predict_classes(X_test_std, verbose=0)
-correct_preds = np.sum(y_test == y_test_pred, axis=0)
-test_acc = correct_preds/y_test.shape[0]
-print('test accuracy:  %0.3f' %(test_acc*100))
-y_org_pred = model.predict_classes(X_org_std, verbose=0)
-correct_preds = np.sum(y_org == y_org_pred, axis=0)
-org_acc = correct_preds/y_org.shape[0]
-print('original accuracy:  %0.3f' %(org_acc*100))
+
+from sklearn.metrics import roc_auc_score
+print('Test ROCAUC score: %.3f' % (roc_auc_score(y_test, y_test_pred)))
+
+from sklearn.metrics import confusion_matrix
+confmat = confusion_matrix(y_true=y_test, y_pred=y_test_pred)
+
+fig, ax = plt.subplots(figsize=(2.5, 2.5))
+ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.5)
+for i in range(confmat.shape[0]):
+    for j in range(confmat.shape[1]):
+        ax.text(x=j, y=i, s=confmat[i, j], va='center', ha='center')
+
+plt.title('Test data confusion matrix')
+plt.xlabel('predicted label')
+plt.ylabel('true label')
+plt.tight_layout()
+plt.show()
 
 print('a')
